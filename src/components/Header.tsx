234d73d536fb9c08652690937d40a9acbe2753b8
Andrey <anbaluxes@gmail.com>
2026-01-23 02:25:41 +0300

    add components Header +  theme (#2)

---
+++ src/components/Header.tsx
@@ -0,0 +1,96 @@
+import SearchIcon from '@mui/icons-material/Search'
+import {
+	AppBar,
+	Box,
+	Button,
+	IconButton,
+	TextField,
+	Toolbar,
+} from '@mui/material'
+import { useState } from 'react'
+
+export function Header() {
+	const [openSearch, setOpenSearch] = useState(false)
+
+	return (
+		<AppBar position='static' sx={{ backgroundColor: 'black' }}>
+			<Toolbar>
+				{/* Иконка-кнопка */}
+				<IconButton
+					edge='start'
+					color='inherit'
+					aria-label='menu'
+					sx={{ mr: 2 }}
+				>
+					<img src='/favicon.svg' alt='Logo' width={50} height={50} />
+				</IconButton>
+
+				{/* Навигация */}
+				<Box sx={{ display: 'flex', gap: 2, flexGrow: 1 }}>
+					<Button
+						color='inherit'
+						sx={{
+							'&:hover': {
+								background: `linear-gradient(90deg,rgba(255, 84, 1, 1) 0%, rgba(252, 84, 0, 1) 50%, rgba(217, 181, 0, 1) 100%)`,
+							},
+						}}
+					>
+						Главная
+					</Button>
+					<Button
+						color='inherit'
+						sx={{
+							'&:hover': {
+								background: `linear-gradient(90deg,rgba(255, 84, 1, 1) 0%, rgba(252, 84, 0, 1) 50%, rgba(217, 181, 0, 1) 100%)`,
+							},
+						}}
+					>
+						Фильмы
+					</Button>
+					<Button
+						color='inherit'
+						sx={{
+							'&:hover': {
+								background: `linear-gradient(90deg,rgba(255, 84, 1, 1) 0%, rgba(252, 84, 0, 1) 50%, rgba(217, 181, 0, 1) 100%)`,
+							},
+						}}
+					>
+						Сериалы
+					</Button>
+					<Button
+						color='inherit'
+						sx={{
+							'&:hover': {
+								background: `linear-gradient(90deg,rgba(255, 84, 1, 1) 0%, rgba(252, 84, 0, 1) 50%, rgba(217, 181, 0, 1) 100%)`,
+							},
+						}}
+					>
+						Фавориты
+					</Button>
+				</Box>
+
+				{/* Поиск */}
+				<Box sx={{ display: 'flex', gap: 1 }}>
+					{openSearch && (
+						<TextField
+							size='small'
+							variant='outlined'
+							placeholder='Поиск...'
+							sx={{ backgroundColor: 'white', borderRadius: 1 }}
+						/>
+					)}
+					<IconButton
+						color='inherit'
+						onClick={() => setOpenSearch(prev => !prev)}
+					>
+						<SearchIcon
+							sx={{
+								fill: `${openSearch ? '#fc5400' : 'white'}`,
+							}}
+						/>
+					</IconButton>
+				</Box>
+			</Toolbar>
+		</AppBar>
+	)
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
+import { ThemeProvider } from '@mui/material/styles'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import './index.css'
+import { theme } from './theme/theme.ts'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<ThemeProvider theme={theme}>
+		<App />
+	</ThemeProvider>,
 )
