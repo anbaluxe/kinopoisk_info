ec9325ab56ea960c65ecd2688741cd2d68c7b675
Andrey Baluhatyy <anbaluxes@gmail.com>
2026-01-23 03:25:20 +0300

    add fetchApi

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,34 @@
+import { useEffect, useState } from 'react'
 import { Header } from './components/Header'
 
+const BASE_URL = import.meta.env.VITE_BASE_URL
+const API_KEY = import.meta.env.VITE_HEADER_API
+
 function App() {
+	const [data, setData] = useState([])
+	useEffect(() => {
+		const fetchApi = async () => {
+			try {
+				const res = await fetch(
+					`${BASE_URL}/movie?field=year&search=2025-2026&sortField=year&sortType=-1&limit=1`,
+					{
+						headers: {
+							'X-API-KEY': API_KEY,
+						},
+					},
+				)
+				if (!res.ok) {
+					throw new Error('API Error')
+				}
+				const data = await res.json()
+				setData(data.docs)
+			} catch (error) {
+				console.log(error)
+			}
+		}
+		// fetchApi()
+	}, [])
+	console.log(data)
 	return (
 		<>
 			<Header />
