b6b15c2b6b83f0dee2ffa330423bafbe1c83bfbd
Andrey Baluhatyy <anbaluxes@gmail.com>
2026-01-23 03:30:31 +0300

    refactor fetchApi in hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,9 @@
-import { useEffect, useState } from 'react'
 import { Header } from './components/Header'
-
-const BASE_URL = import.meta.env.VITE_BASE_URL
-const API_KEY = import.meta.env.VITE_HEADER_API
+import { useFetchNew } from './hooks/useFetchNew'
 
 function App() {
-	const [data, setData] = useState([])
-	useEffect(() => {
-		const fetchApi = async () => {
-			try {
-				const res = await fetch(
-					`${BASE_URL}/movie?field=year&search=2025-2026&sortField=year&sortType=-1&limit=1`,
-					{
-						headers: {
-							'X-API-KEY': API_KEY,
-						},
-					},
-				)
-				if (!res.ok) {
-					throw new Error('API Error')
-				}
-				const data = await res.json()
-				setData(data.docs)
-			} catch (error) {
-				console.log(error)
-			}
-		}
-		// fetchApi()
-	}, [])
-	console.log(data)
+	const movie = useFetchNew(2026)
+	console.log(movie)
 	return (
 		<>
 			<Header />
