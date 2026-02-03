bd107d9e51003af394f1d2b19352f9314ce8614d
Andrey Baluhatyy <anbaluxes@gmail.com>
2026-02-03 08:27:12 +0300

    add 404

--- src/widgets/layout/ui/Layout.tsx
+++ src/widgets/layout/ui/Layout.tsx
@@ -1,10 +1,14 @@
 import { Header } from '@/widgets/header/ui/Header'
-import { Outlet } from 'react-router'
+import { Outlet, useMatches } from 'react-router'
 
 export const Layout = () => {
+	const matches = useMatches()
+	const hideHeader = matches.some(
+		match => typeof match.handle === 'object' && match.handle?.hideHeader,
+	)
 	return (
 		<>
-			<Header />
+			{!hideHeader && <Header />}
 			<main>
 				<Outlet />
 			</main>
